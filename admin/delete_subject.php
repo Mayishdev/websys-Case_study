55ca86baf2a0f14d1fc021e69684658c62a45d8b
Mayishdev <142711918+Mayishdev@users.noreply.github.com>
2025-12-14 21:05:09 +0800

    Delete admin directory

--- admin/delete_subject.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-include '../db.php';
-if ($_SESSION['role'] != 'admin') header("Location: ../login.php");
-
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM subjects WHERE id=$id");
-header("Location: list_subjects.php");
